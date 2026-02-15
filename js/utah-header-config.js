baa6ffa3c260d05ecba8389fab0f3c47f0073f89
Zach Lewis <gonzoga56@gmail.com>
2026-02-14 21:35:14 -0700

    Cleanup unused files and fix mobile layout issues

--- js/utah-header-config.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('utahHeaderLoaded', () => {
-  window["@utahdts/utah-design-system-header"].setUtahHeaderSettings({
-    footer: {
-      linkTextPrivacyPolicy: 'Privacy Policy',
-      linkPrivacyPolicy: 'https://dhhs.utah.gov/website-privacy/',
-      showHorizontalRule : 'true'
-    }
-  });
-});
